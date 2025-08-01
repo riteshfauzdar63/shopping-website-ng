62199668c1a2f3d893a69ce593e533b59b47237e
riteshfauzdar63 <rfauzdar6@gmail.com>
2025-08-01 15:23:29 +0530

    remove the unnecassary code from main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { LocalStorageSessionDataService } from './service/local-storage-session-data.service';
 import { CommonModule } from '@angular/common';
-import { CartIconValueService } from './service/cart-icon-value.service';
 
-// import { LocalStorageSessionDataService } from './service/local-storage-session-data.service';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +15,7 @@ export class AppComponent implements OnInit {
 
   cartItemValue : number = 0;
   constructor(
-              private localdataservice : LocalStorageSessionDataService){  
+    private localdataservice : LocalStorageSessionDataService){  
   }
 
   ngOnInit(): void {
